9237b52df594a4fddf0eedc0ba0be606e077b794
Debojyoti Banerjee <debojyotibanerjee2001@gmail.com>
2025-09-10 13:07:40 +0530

    Implemenet save and load method in model.py

--- app/model.py
+++ app/model.py
@@ -1,12 +1,23 @@
 from sklearn.linear_model import LogisticRegression
 from app.exceptions import modelError
+import pickle
 
 
 class model:
     def __init__(self):
         self.ml_model=LogisticRegression()
-    def get_model(self):
+        
+        
+    def save(self,path="model.pkl"):
         try:
-            return self.ml_model
+            with open(path,"wb") as f:
+                pickle.dump(self.ml_model,f)
+        except Exception as e:
+            raise modelError(str(e))
+        
+    def load(self,path="model.pkl"):
+        try:
+            with open(path,"rb") as f:
+                self.ml_model=pickle.load(f)
         except Exception as e:
             raise modelError(str(e))
