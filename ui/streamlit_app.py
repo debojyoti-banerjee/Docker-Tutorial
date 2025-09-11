de81c6ba4fe18b3dd47e491a9c7451281d68e10d
Debojyoti Banerjee <debojyotibanerjee2001@gmail.com>
2025-09-11 17:49:58 +0530

    Implemented ui

---
+++ ui/streamlit_app.py
@@ -0,0 +1,25 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+import streamlit as st
+from app.predictor import predictor
+
+st.title("Iris Predictor App")
+
+st.header("Make a Prediction")
+
+sepal_length=st.text_input("Sepal-Length")
+sepal_width=st.text_input("Sepal-Width")
+petal_length=st.text_input("Petal-Length")
+petal_width=st.text_input("Petal-Width")
+
+if st.button("Predict"):
+    try:
+        prediction=predictor()
+        features=[sepal_length,sepal_width,petal_length,petal_width]
+        result=prediction.predict(features)
+        st.success(f"Predicted class is {result}")
+    except Exception as e:
+        print(str(e))
+        st.error("Some Error occured")
