a6dfcb0021585aec2cc4328b23fe4cc4212339bd
Debojyoti Banerjee <debojyotibanerjee2001@gmail.com>
2025-09-12 16:31:31 +0530

    learned about docker volume containor management docker hub

--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -4,8 +4,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import streamlit as st
 from app.predictor import predictor
+from app.trainer import trainer
 
 st.title("Iris Predictor App")
+
+if st.button("Train model"):
+    trainer=trainer()
+    trainer.train_and_save()
+    st.success("Model retrained and save")
 
 st.header("Make a Prediction")
 
